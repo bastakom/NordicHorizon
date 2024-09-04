55a78037dbf776bedba85da03c99e251407678a7
BK <philip@bastakompisar.se>
2024-09-04 16:44:09 +0200

    removed resend

--- app/api/form/route.ts
+++ app/api/form/route.ts
@@ -1,31 +1,31 @@
-import { Resend } from 'resend'
+// import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+// const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(req: Request) {
-  const { name, email, message } = await req.json()
+// export async function POST(req: Request) {
+//   const { name, email, message } = await req.json()
 
-  const messageBody = `
-    <div style="background-color: #f9f9f9; padding: 20px;">
-      <p>Meddelande från: ${name}</p>
-      <h3>Email: ${email}</h3>
-      <p>Meddelande: ${message}</p>
-    </div>
-  `
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Notifikation: BK-Kontakt <onboarding@resend.dev>',
-      to: ['info@bastakompisar.se'],
-      subject: 'Notifikation från BK kontakt',
-      html: messageBody,
-    })
+//   const messageBody = `
+//     <div style="background-color: #f9f9f9; padding: 20px;">
+//       <p>Meddelande från: ${name}</p>
+//       <h3>Email: ${email}</h3>
+//       <p>Meddelande: ${message}</p>
+//     </div>
+//   `
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'Notifikation: BK-Kontakt <onboarding@resend.dev>',
+//       to: ['info@bastakompisar.se'],
+//       subject: 'Notifikation från BK kontakt',
+//       html: messageBody,
+//     })
 
-    if (error) {
-      return Response.json({ error }, { status: 500 })
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 })
+//     }
 
-    return Response.json(data)
-  } catch (error) {
-    return Response.json({ error }, { status: 500 })
-  }
-}
+//     return Response.json(data)
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 })
+//   }
+// }
