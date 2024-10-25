6bd683dc01ae7688df166519021855e0c40b2a35
Philip <anlanderp@gmail.com>
2024-10-25 13:49:13 +0200

    error fix

--- app/components/Form/Form.tsx
+++ app/components/Form/Form.tsx
@@ -1,43 +1,3 @@
-import { Resend } from 'resend'
-
-const usermail = process.env.MARKNAD_EMAIL
-const resend = new Resend(process.env.RESEND_API_KEY)
-
-export async function POST(req: Request) {
-  const { name, email, message, phone } = await req.json()
-
-  const messageBody = `
-    <div style="background-color: #f9f9f9; padding: 20px;">
-      <p>Meddelande från: ${name}</p>
-      <h3>Email: ${email}</h3>
-      <p>tel: ${phone}</p>
-      <p>Meddelande: ${message}</p>
-    </div>
-  `
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Notifikation: Kontakt Form <onboarding@resend.dev>',
-      to: [`${usermail}`],
-      subject: `Notifikation: Customer ${name}`,
-      html: messageBody,
-    })
-
-    if (error) {
-      return Response.json({ error }, { status: 500 })
-    }
-
-    return Response.json(data)
-  } catch (error) {
-    return Response.json({ error }, { status: 500 })
-  }
-}
-
-
-
-
-------------------------
-
-
 "use client";
 
 import { useState } from "react";
@@ -82,7 +42,6 @@ const Form = ({ title }: any) => {
     }
   };
 
-  // Testing form
 
   return (
     <div className="py-20 flex flex-col w-full justify-center items-center">
