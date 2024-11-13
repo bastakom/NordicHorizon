7b7ea212a042406c4abb7e4709aca9fc6257b537
Philip <anlanderp@gmail.com>
2024-11-13 15:15:22 +0100

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,15 @@ import Footer from "./components/Footer/Footer";
 import dynamic from "next/dynamic";
 import StoryblokProvider from "@/components/StoryblokProvider";
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
-
-
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 storyblokInit({
   accessToken: process.env.STORYBLOCK_API_TOKEN,
   use: [apiPlugin],
   apiOptions: {
-    region: 'eu',
+    region: "eu",
   },
-})
+});
 
 export default function RootLayout({
   children,
@@ -24,6 +23,22 @@ export default function RootLayout({
   return (
     <StoryblokProvider>
       <html>
+        <head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-98B6G3X5TN"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-98B6G3X5TN');
+            `,
+            }}
+          />{" "}
+        </head>
         <body>
           <Header />
           <main>{children}</main>
