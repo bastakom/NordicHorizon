e3f7dceec3de626617739d7c6c43d039efaa027c
Philip <anlanderp@gmail.com>
2025-01-22 11:57:57 +0100

    kontakt & nav

--- app/components/Team/Team.tsx
+++ app/components/Team/Team.tsx
@@ -1,34 +1,45 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-  data: {
-    noimages: any;
-    img_1: { filename: string; id: string };
-    img_2: { filename: string; id: string };
-    img_3: { filename: string; id: string };
-    title_1: string;
-    title_2: string;
-    title_3: string;
-    name_1: string;
-    name_2: string;
-    name_3: string;
-    mail_1: string;
-    mail_2: string;
-    mail_3: string;
-    phone_1: string;
-    phone_2: string;
-    phone_3: string;
-  };
+  data: any;
 }
 
 const Team = ({ data }: Props) => {
   return (
-    <div>
-      <div className="h-[40vh] text-center w-full bg-[#16364D] flex justify-center items-center">
+    <div className="mb-20">
+      <div className="py-20 lg:py-0 lg:h-[40vh] text-center w-full bg-[#16364D] flex justify-center items-center">
         <h2 className="text-white text-[25px]">KONTAKT</h2>
       </div>
-      <div className="flex flex-wrap m-auto justify-center  px-5 lg:px-20 pt-20 gap-40  pb-32 bg-[#E9EFED]">
+      <div className="lg:grid-cols-4 gap-4 grid mt-5 lg:mt-0 container mx-auto">
+        {data.contact_fields.map((data: any) => {
+          return (
+            <div className="flex justify-center flex-col text-center gap-5  border lg:border-none border-black lg:p-0 p-5 mx-2 lg:mx-0">
+              {!data.noimages && (
+                <div className="relative h-[400px] w-[320px] m-auto -mt-0 lg:-mt-32">
+                  <Image
+                    src={data.profile_picture?.filename}
+                    alt={data.profile_picture?.id}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              )}
+              <div>
+                <h2 className="text-[20px]">{data.name}</h2>
+                <h3 className="text-[16px]">{data.title}</h3>
+              </div>
+              <div className="flex flex-col">
+                <Link href={`mailto:${data.mail_1}`}>{data.mail}</Link>
+                <Link href={`phone:${data.phone_1}`}>{data.phone}</Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {/* <div className="flex flex-wrap m-auto justify-center  px-5 lg:px-20 pt-20 gap-40  pb-32 bg-[#E9EFED]">
         <div className="flex justify-center flex-col text-center gap-5">
           {!data.noimages && (
             <div className="relative h-[400px] w-[320px] m-auto -mt-0 lg:-mt-48">
@@ -49,47 +60,7 @@ const Team = ({ data }: Props) => {
             <Link href={`phone:${data.phone_1}`}>{data.phone_1}</Link>
           </div>
         </div>
-        <div className="flex justify-center flex-col text-center gap-5">
-          {!data.noimages && (
-            <div className="relative h-[400px] w-[320px] m-auto -mt-0 lg:-mt-48">
-              <Image
-                src={data.img_2.filename}
-                alt={data.img_2.id}
-                fill
-                className="object-cover"
-              />
-            </div>
-          )}
-          <div>
-            <h2 className="text-[20px]">{data.name_2}</h2>
-            <h3 className="text-[16px]">{data.title_2}</h3>
-          </div>
-          <div className="flex flex-col">
-            <Link href={`mailto:${data.mail_2}`}>{data.mail_2}</Link>
-            <Link href={`phone:${data.phone_2}`}>{data.phone_2}</Link>
-          </div>
-        </div>
-        <div className="flex justify-center flex-col text-center gap-5">
-          {!data.noimages && (
-            <div className="relative h-[400px] w-[320px] m-auto -mt-0 lg:-mt-48">
-              <Image
-                src={data.img_3.filename}
-                alt={data.img_3.id}
-                fill
-                className="object-cover"
-              />
-            </div>
-          )}
-          <div>
-            <h2 className="text-[20px]">{data.name_3}</h2>
-            <h3 className="text-[16px]">{data.title_3}</h3>
-          </div>
-          <div className="flex flex-col">
-            <Link href={`mailto:${data.mail_3}`}>{data.mail_3}</Link>
-            <Link href={`phone:${data.phone_3}`}>{data.phone_3}</Link>
-          </div>
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 };
