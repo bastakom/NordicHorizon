e695fc15687451fff8c942d30663a45ea8937a93
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-16 13:23:17 +0200

    fix faq-block

--- components/ui/faq-block.tsx
+++ components/ui/faq-block.tsx
@@ -5,11 +5,11 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 
 export const FaqBlock = ({ res }: any) => {
   return (
-    <div className="lg:max-w-[80%] px-5 lg:px-14 py-10 lg:py-0  lg:pb-10 lg:mx-auto">
+    <div className="lg:max-w-[80%] px-5 lg:px-14 py-10 lg:py-0  lg:pb-10 lg:mx-auto lg:mt-20">
       <div className="lg:flex  flex-row-reverse gap-10 cursor-pointer">
         <div className="lg:w-1/3">
-          {res?.story?.content?.FAQ_content?.map((item: any) => (
-            <div key={item._uid}>
+          {res?.story?.content?.FAQ_content?.map((item: any, index: number) => (
+            <div key={index}>
               {item?.faq_field?.map((i: any) => (
                 <div className="flex items-center gap-2 bg-[#E9EFED] mb-4 p-4 rounded-md w-[100%]">
                   <MdKeyboardArrowDown fontSize={30} />
